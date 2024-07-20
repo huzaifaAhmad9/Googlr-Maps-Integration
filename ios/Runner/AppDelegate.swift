7e23976c1b32520b8415a4ebe722aca5882bd5ff
Huzaifa_073 <huzaifaidrees803@gmail.com>
2024-07-21 02:42:33 +0500

    setting project with google map api console

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
-import UIKit
+import UIKitimport
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+  GMSServices.provideAPIKey("AIzaSyAbEwS-6kyBW66udi_E6PnlyTtTHg3NsWA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
