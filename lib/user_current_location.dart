215af5c76ad6e6c5db6f433f49a7272585eaafdf
Huzaifa_073 <huzaifaidrees803@gmail.com>
2024-07-22 02:57:20 +0500

    get user current loc Completed!!!

--- lib/user_current_location.dart
+++ lib/user_current_location.dart
@@ -29,7 +29,7 @@ class _UserCurrentLocationState extends State<UserCurrentLocation> {
 
 
 //
-  final List<Marker> _markers = const <Marker> [
+  final List<Marker> _markers =  <Marker> [
     Marker(
         markerId: MarkerId('1'),
       position: LatLng(31.41918682187527, 74.23039392630363,),
@@ -62,11 +62,27 @@ class _UserCurrentLocationState extends State<UserCurrentLocation> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          getUserCurrentlocation().then((value){
-            print('Mu Current Location');
+          getUserCurrentlocation().then((value) async {
+            print('My Current Location');
             print(value.latitude.toString() + " " + value.longitude.toString());
-            
 
+
+            _markers.add(
+              Marker(
+                  markerId: MarkerId('2'),
+                position: LatLng(value.latitude , value.longitude),
+                infoWindow: InfoWindow(title: "My Current Location")
+              )
+            );
+            CameraPosition cameraPosition = CameraPosition(
+              zoom: 14,
+              target:  LatLng(value.latitude , value.longitude),
+            );
+            final GoogleMapController controller = await _controller.future;
+            controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+            setState(() {
+
+            });
           });
         },
         child: const Icon(Icons.local_activity),
