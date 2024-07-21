a9d4d5d4e2b514bd0fc1d40db72b9a6f67bee3b1
Huzaifa_073 <huzaifaidrees803@gmail.com>
2024-07-22 00:55:21 +0500

    Camera Animation

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,26 +8,75 @@ class HomeScreen extends StatefulWidget {
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
-
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   Completer<GoogleMapController> _controller = Completer();
+
   static final CameraPosition _kGooglePlex = const CameraPosition(
-      target: LatLng(31.41918682187527, 74.23039392630363,),
-    zoom: 14,
+    target: LatLng(
+      31.41918682187527,
+      74.23039392630363,
+    ),
+    zoom: 15,
   );
+
+  List<Marker> _marker = [];
+  final List<Marker> _list = [
+    Marker(
+      markerId: MarkerId('1'),
+      position: LatLng(
+        31.41918682187527,
+        74.23039392630363,
+      ),
+      infoWindow: InfoWindow(title: 'My University'),
+    ),
+    Marker(
+      markerId: MarkerId('2'),
+      position: LatLng(31.587703123698308, 74.32831329606834),
+      infoWindow: InfoWindow(title: 'My Home'),
+    ),
+    Marker(
+      markerId: MarkerId('3'),
+      position: LatLng(60.17050166223287, 24.924755470312558),
+      infoWindow: InfoWindow(title: 'Finland'),
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _marker.addAll(_list);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
+      body: SafeArea(
+        child: GoogleMap(
           initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller){
+          onMapCreated: (GoogleMapController controller) {
             _controller.complete(controller);
+          },
+          mapType: MapType.normal,
+          compassEnabled: true,
+          myLocationEnabled: true,
+          markers: Set<Marker>.of(_marker),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          GoogleMapController controller = await _controller.future;
+          controller
+              .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+            target: LatLng(60.17050166223287, 24.924755470312558),
+            zoom: 14
+          )));
+          setState(() {
+
+          });
         },
-        mapType: MapType.normal,
-        compassEnabled: true,
-        myLocationEnabled: true,
+        child: Icon(Icons.location_disabled_outlined),
       ),
     );
   }
